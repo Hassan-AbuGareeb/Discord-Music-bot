1e266eaca340d0f613ea3e2506d910edde9d926b
Hassan <hassan.abugareeb@gmail.com>
2025-01-18 17:23:05 +0300

    added basic implemetation for a middleware

---
+++ src/middleware/utilityMiddleware.js
@@ -0,0 +1,10 @@
+const appendCommandToRoute = (req, res, next) =>{
+    //access the command name from the request body data
+    //append to the url
+    // req.url = commandName
+    // next();
+}
+
+module.exports = {
+    appendCommandToRoute
+}
--- src/server.js
+++ src/server.js
@@ -3,10 +3,13 @@ const app = require("express")();
 const cors = require("cors");
 const bodyParser = require("express").json;
 const commandsRoute = require ("./routes/commandsRouter");
+const { appendCommandToRoute } = require("./middleware/utilityMiddleWare");
 
 //enable libraries and register routes on the server app
 app.use(cors());
 app.use(bodyParser());
-app.use("/", commandsRoute);
+app.use("/interactions", appendCommandToRoute, commandsRoute);
+
+
 
 module.exports = app;
