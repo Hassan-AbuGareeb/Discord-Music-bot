bf3d687dab797366ed62d0c89ab92bbc9540e962
Hassan <hassan.abugareeb@gmail.com>
2025-01-18 16:44:11 +0300

    added basic commands stubs

--- src/controllers/commandsController.js
+++ src/controllers/commandsController.js
@@ -1,7 +1,37 @@
-const GetCommand = (req, res) =>{
+const testCommand = (req, res) =>{
     res.send("America yaaa !")
 }
 
+const playCommand = (req, res) => {
+    //extract song name
+    //search it on spotify
+    //get its url
+    //download / load it to memory with dl package
+    //tell bot to enter channel and play it
+    res.send("playing song")
+}
+
+const stopCommand = (req, res) => {
+    //stop song
+    //can't be resumed
+    //move bot out?
+    res.send("song stopped")
+}
+
+const pauseCommand = (req, res) => {
+    //only pause if playing a song
+    res.send("song paused")
+}
+
+const resumeCommand = (req, res) => {
+    //resume current song
+    res.send("song resumed")
+}
+
 module.exports = {
-    GetCommand
+    testCommand,
+    playCommand,
+    stopCommand,
+    pauseCommand,
+    resumeCommand
 }
--- src/routes/commandsRouter.js
+++ src/routes/commandsRouter.js
@@ -1,8 +1,18 @@
 require("../controllers/commandsController")
 const express = require('express');
-const { GetCommand } = require("../controllers/commandsController");
+const {
+    testCommand,
+    playCommand,
+    stopCommand,
+    pauseCommand,
+    resumeCommand
+ } = require("../controllers/commandsController");
 const router = express.Router();
 
-router.get('/', GetCommand)
+router.get('/', testCommand);
+router.post('/play', playCommand);
+router.get('/stop', stopCommand);
+router.get('/pause', pauseCommand);
+router.get('/resume', resumeCommand);
 
 module.exports = router;
